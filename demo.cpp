73b4b949fb05503df8050dc4f3e7cf7104f63df8
Ben <dreamvb@outlook.com>
2021-05-31 20:59:40 +0100

    Update

--- demo.cpp
+++ demo.cpp
@@ -1,28 +1,27 @@
-//Simple Queue class using a linked list
+//Simple Genetic Queue class using a linked list.
+//if you like this see my stack Implementation
+//https://github.com/DreamVB/LStack
+
 #include <iostream>
 #include "LQue.h"
 using namespace std;
 
 int main(){
 
-	LQueue q;
-	int x = 100;
-	//Put some items on the queue 100 to 800
-	while (x <= 800){
-		//Add item
-		q.enqueue(x);
-		//INC by 100
-		x += 100;
-	}
+	LQueue<int> q;
+	int myint;;
+
+	std::cout << "Please enter some integers (enter 0 to end):" << std::endl;
+	do {
+		std::cin >> myint;
+		q.enqueue(myint);
+	} while (myint);
+
 	//Display the items in the queue that were added.
-	while (!q.isEmpty()){
+	while (!q.empty()){
 		//Output item on que
 		std::cout << q.dequeue() << endl;
 	}
-	//Pop one more item this will disp;ay an error messageing
-	//tellings us the queue is empty
-	std::cout << q.dequeue() << endl;
 
-	system("pause");
-	return 0;
+	return EXIT_SUCCESS;
 }
